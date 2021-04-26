55c3018641a7561ea56c21887e64779b4297afaa
yuta77723 <hayakawamon714@gmail.com>
2021-04-26 21:49:02 +0900

    2章チェックテスト修正
    Check.javaの修正

--- JavaStudy/Cheack2/src/check/Check.java
+++ JavaStudy/Cheack2/src/check/Check.java
@@ -4,23 +4,22 @@ import constants.Constants;
 
 public class Check {
 	
-	private String firstName = "早川";
-	private String lastName = "雄太";
-	
-	private void printName() {
-		System.out.println("printNameメソッド → " + this.firstName + this.lastName);
-	}
+	private static String firstName = "早川";
+	private static String lastName = "雄太";
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		
-		Check check = new Check();
-		check.printName();
+		printName(firstName, lastName);
 		
 		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
 		pet.introduce();
 		
 		RobotPet robotPet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
 		robotPet.introduce();
+	}
+	
+	private static void printName(String firstName, String lastName) {
+		System.out.println("printNameメソッド → " + firstName + lastName);
 	}
 }
