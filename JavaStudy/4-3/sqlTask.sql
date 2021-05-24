2d686fa0dfc2e2b1888750f3d1a4c9a4fa474cf0
yuta77723 <hayakawamon714@gmail.com>
2021-05-25 00:17:03 +0900

    4-3修正

--- JavaStudy/4-3/sqlTask.sql
+++ JavaStudy/4-3/sqlTask.sql
@@ -9,7 +9,7 @@
  
  
 -- 3. 在庫テーブルに店舗テーブル、商品テーブルを「内部結合」し、店舗名・商品名・在庫数を全て取得しなさい。
--- [SELECT * FROM stock_table
+-- [SELECT store_name, goods_name, quantity FROM stock_table
 	JOIN store_table ON stock_table.store_code = store_table.store_code
 	JOIN goods_table ON stock_table.goods_code = goods_table.goods_code]
  
